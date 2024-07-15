10b5d77160f30e96132bde9a75b9b49bd29d4478
loveaoe33 <loveaoe33@gmail.com>
2024-07-15 17:59:16 +0800

    20240715

--- app/orclient/orclient_tv/components/orclient_head.tsx
+++ app/orclient/orclient_tv/components/orclient_head.tsx
@@ -9,6 +9,33 @@ import logo from "../image/logo.jpg";
 
 const Orclient_Head: React.FC=(props)=>{
   const [currentDate,setCurrentDateTime] =useState<Date>(new Date());
+  const day=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const daysWeek=[currentDate.getDay()];
+
+  const caseDay = (dayOfWeek) => {
+  switch (dayOfWeek) {
+    case "Sunday":
+      return "星期日";
+    case "Monday":
+      return "星期一";
+    case "Tuesday":
+      return "星期二";
+    case "Wednesday":
+      return "星期三";
+    case "Thursday":
+      return "星期四";
+    case "Friday":
+      return "星期五";
+    case "Saturday":
+      return "星期六";
+    default:
+      return ""; // 如果沒有匹配到任何情況，可以返回空字符串或者其他預設值
+  }
+};
+
+  const test=()=>{
+    alert(caseDay(daysWeek));
+  }
   useEffect(()=>{
     const time=setInterval(()=>{
       setCurrentDateTime(new Date);
@@ -17,7 +44,7 @@ const Orclient_Head: React.FC=(props)=>{
   },[]);
 
 
-  const formattedTime=`${currentDate.toLocaleDateString()}-${currentDate.toLocaleTimeString()}`;
+  const formattedTime=`${currentDate.toLocaleDateString()}-${caseDay(caseDay(daysWeek))}-${currentDate.toLocaleTimeString()}`;
   return (
     <main><div className="Orclient_Head_Left"    ><Image className="Logo" src={logo} alt="Logo" width={100} height={80} /><span className="Left_Text">重仁骨科醫院<br/></span><span className="Left_English">(Chugnjen-Orthopaedic-Hospital)</span></div><div className="Orclient_Head_Right">{formattedTime}</div></main>    
 
--- app/orclient/orclient_tv/components/orclient_middle.tsx
+++ app/orclient/orclient_tv/components/orclient_middle.tsx
@@ -9,7 +9,7 @@ import YouTube from 'react-youtube';
 
 const Orclient_Middle: React.FC=(props)=>{
   return (
-    <main><div className="Orclient_Middle_Left"><iframe width="560" height="315" src="https://www.youtube.com/watch?v=8DYQctIo5s0" title="YouTube video player" frameBorder="0" allowFullScreen ></iframe><h1>this is or Orclient_Middle</h1></div><div className="Orclient_Middle_Right">555</div></main>    
+    <main><div className="Orclient_Middle_Left"><iframe width="950" height="680" src="https://www.youtube.com/embed/oIgbl7t0S_w?&autoplay=1" title="【#中天新聞不斷電直播Live】 CTI中天新聞24小時HD新聞直播 │ CTITV Taiwan News HD Live｜台湾のHDニュース放送｜ 대만 HD 뉴스 방송 @中天電視CtiTv" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div><div className="Orclient_Middle_Right">555</div></main>    
 
   );
 
