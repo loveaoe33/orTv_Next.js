eb281aec2398a8b63ee4b5b898a983ebf8268b42
loveaoe33 <loveaoe33@gmail.com>
2025-07-07 21:07:20 +0800

    20250707-3

--- app/orclient/page.tsx
+++ app/orclient/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><div><h1>this is or client</h1></div><div>hello2</div></main>    
+    <main><div><h1>this is or client</h1></div><div>helldo2</div></main>    
 
   );
 }
